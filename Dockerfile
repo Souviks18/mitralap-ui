9f77132db4955d01333446ef327499562b00e64f
Souvik Ghosh <215896465+Souviks18@users.noreply.github.com.>
2025-06-18 23:28:04 +0530

    🎯 Clean working mitralap-ui setup

--- Dockerfile
+++ Dockerfile
@@ -1,15 +1,11 @@
 FROM python:3.10-slim
 
-ENV PYTHONDONTWRITEBYTECODE=1
-ENV PYTHONUNBUFFERED=1
-
 WORKDIR /app
 
-COPY . /app/
+COPY . /app
 
 RUN pip install --no-cache-dir -r requirements.txt
 
 EXPOSE 5000
 
-CMD ["python", "app.py"]
-
+CMD ["gunicorn", "-b", "0.0.0.0:5000", "app:app"]
--- app.py
+++ app.py
@@ -3,8 +3,6 @@ import requests
 
 app = Flask(__name__)
 
-
-
 @app.route("/")
 def home():
     return render_template("index.html")
@@ -13,18 +11,14 @@ def home():
 def chat():
     user_message = request.json.get("message")
     rasa_url = "https://mitralap-bot.onrender.com/webhooks/rest/webhook"
-
-
     payload = {"message": user_message}
     response = requests.post(rasa_url, json=payload)
 
     messages = []
     if response.status_code == 200:
-        responses = response.json()
-        for r in responses:
+        for r in response.json():
             messages.append(r.get("text"))
     return jsonify({"responses": messages})
 
 if __name__ == "__main__":
-    app.run(host="0.0.0.0", port=5000, debug=True)
-
+    app.run(host="0.0.0.0", port=5000)
